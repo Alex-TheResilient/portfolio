75b36c353177604b1ad3d8ef427fa46c760ab336
Alex Huaracha <129701149+Alex-TheResilient@users.noreply.github.com>
2025-07-28 15:58:04 -0500

    Refactor Header component layout and styles; enhance theme toggle integration

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,17 +1,26 @@
-import { Dumbbell, ChevronRight, ChevronLeft } from 'lucide-react';
+import { Dumbbell, ChevronRight } from 'lucide-react';
+import ThemeToggle from './ThemeToggle';
+
 export default function Header() {
   return (
-    <header className="flex items-center justify-between py-4 px-4">
+    <header className="flex items-center justify-between py-4 px-16">
       {/* Logo and Name */}
-      <a href="/" className="flex items-center">
+      <a href="/" className="flex flex-col text-black dark:text-white">
         <div className="flex items-center text-4xl group">
-          <ChevronLeft className="w-8 h-8 text-blue-500" />
-          <Dumbbell className="w-8 h-8 text-red-500" />
+          <Dumbbell className="w-6 h-6 text-red-500" />
           <ChevronRight className="w-8 h-8 text-cyan-500 -ml-2" />
+          <span className="text-xl font-bold -ml-2">Alex Huaracha</span>
         </div>
-        <span className="text-2xl font-bold">Alex Huaracha</span>
+        <div>
+          <p className="text-xs font-display-pacifico ">
+            "If you can imagine it, you can code it."
+          </p>
+        </div>
       </a>
       {/* Theme Toggle */}
+      <div className="flex items-center space-x-4">
+        <ThemeToggle />
+      </div>
     </header>
   );
 }
