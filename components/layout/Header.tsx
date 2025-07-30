85e5b73212c940bd7537e37edcdeaf3e1982c568
Alex Huaracha <129701149+Alex-TheResilient@users.noreply.github.com>
2025-07-29 20:14:35 -0500

    Refactor Header component; replace anchor with Link for improved routing and update HeroSection layout; enhance profile card structure and avatar display

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,12 +1,13 @@
 import { Dumbbell, ChevronRight } from 'lucide-react';
 import ThemeToggle from './ThemeToggle';
+import Link from 'next/link';
 
 export default function Header() {
   return (
     <div className="container">
       <header className="flex h-18 items-center justify-between">
         {/* Logo and Name */}
-        <a href="/" className="flex-col text-black dark:text-white">
+        <Link href="/" className="flex-col text-black dark:text-white">
           <div className="flex items-center text-4xl group">
             <Dumbbell className="w-7 h-7 text-red-500" />
             <ChevronRight className="w-9 h-9 text-cyan-500 -ml-2" />
@@ -14,10 +15,10 @@ export default function Header() {
           </div>
           <div>
             <p className="text-xs font-display-pacifico ">
-              "If you can imagine it, you can code it."
+              {'"If you can imagine it, you can code it."'}
             </p>
           </div>
-        </a>
+        </Link>
         {/* Theme Toggle */}
         <ThemeToggle />
       </header>
--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -1,76 +1,90 @@
 import { personalInfo, technologies } from '@/lib/data';
 import { Phone, Mail, MapPin } from 'lucide-react';
+import Image from 'next/image';
 
 export default function HeroSection() {
   return (
     <div className="flex h-screen flex-col items-center justify-center">
-      <div className="container relative z-10 mx-auto max-w-full space-y-8 xl:px-16">
+      <div className="container space-y-8 xl:px-16">
         <div className="min-h-[calc(100vh-56px)] items-center space-y-8 md:flex md:space-y-0">
           <div className="grid items-center gap-x-16 md:grid-cols-2 xl:gap-x-28">
-            {/* Card */}
-            <div className="backdrop-blur-md relative rounded-2xl shadow-lg w-96 md:w-[450px] lg:w-[600px] overflow-hidden border border-white/20 dark:border-gray-700/30 [--banner-height:10rem] md:[--banner-height:12rem] lg:[--banner-height:14rem] [--avatar-size:9rem] md:[--avatar-size:10rem] lg:[--avatar-size:10rem]">
-              {/* Banner */}
-              <div className="bg-black h-[var(--banner-height)]"></div>
+            {/* Profile Card */}
+            <div className="relative overflow-hidden rounded-3xl border border-zinc-200 shadow-sm backdrop-blur-2xl dark:border-zinc-800 dark:text-zinc-50">
+              {/* Banner with cover image */}
+              <div className="relative aspect-[3/1] overflow-hidden before:absolute before:inset-0 before:bg-[image:url(/images/cover.avif)] before:bg-cover before:bg-center after:absolute after:bottom-0 after:left-0 after:z-[1] after:h-px after:w-full after:bg-white/20" />
 
               {/* Avatar */}
-              <div className="size-[var(--avatar-size)] absolute top-[calc(var(--banner-height)-var(--avatar-size)/2)] left-4 rounded-full overflow-hidden border-4 border-white/50 dark:border-gray-600/50 shadow-lg">
-                <img src="/images/avatar.png" alt={personalInfo.name} />
+              <div className="relative left-[5px] z-[1] -mt-12 h-48 px-8 lg:-mt-24 lg:mb-6">
+                <Image
+                  src="/images/avatar.avif"
+                  alt={personalInfo.name}
+                  width={192}
+                  height={192}
+                  className="rounded-full"
+                />
               </div>
 
-              {/* Personal Info */}
-              <div className="pt-[calc(var(--avatar-size)/2+1rem)] space-y-4 px-4 pb-6">
-                <div className="flex items-center gap-2">
-                  <h1 className="text-3xl md:text-4xl font-bold text-gray-800 dark:text-white">
-                    {personalInfo.name}
-                  </h1>
-                </div>
+              {/* Profile Info */}
+              <div className="px-8 pb-6 space-y-6">
+                {/* Name */}
+                <h1 className="text-3xl md:text-4xl font-mono font-bold text-gray-800 dark:text-white">
+                  {personalInfo.name}
+                </h1>
 
-                <div className="space-y-3 text-gray-600 text-base dark:text-white">
-                  <p className="flex items-center gap-2">
-                    <Mail className="w-5 h-5" />
-                    {personalInfo.email}
-                  </p>
-                  <p className="flex items-center gap-2">
-                    <MapPin className="w-5 h-5" />
-                    {personalInfo.location}
-                  </p>
-                  <p className="flex items-center gap-2">
-                    <Phone className="w-5 h-5" />
-                    {personalInfo.phone}
-                  </p>
+                {/* Contact Info */}
+                <div className="space-y-2 text-sm md:text-base font-mono">
+                  {[
+                    {
+                      icon: Mail,
+                      content: (
+                        // eslint-disable-next-line @next/next/no-html-link-for-pages
+                        <a href={`mailto:${personalInfo.email}`}>
+                          {personalInfo.email}
+                        </a>
+                      ),
+                    },
+                    { icon: MapPin, content: personalInfo.location },
+                    { icon: Phone, content: personalInfo.phone },
+                  ].map(({ icon: Icon, content }, index) => (
+                    <div key={index} className="flex items-center">
+                      <Icon className="w-5 h-5 mr-4 text-zinc-500 shrink-0" />
+                      {content}
+                    </div>
+                  ))}
                 </div>
               </div>
             </div>
 
-            {/* Presentation */}
+            {/* About Section */}
             <div className="space-y-8 dark:text-slate-50">
+              {/* Greeting */}
               <div className="space-y-2 text-center hidden md:block">
-                <div className="select-none bg-gradient-to-t from-zinc-950 to-zinc-600 bg-clip-text pt-1 font-display-pacifico text-8xl text-transparent dark:from-zinc-500 dark:to-white">
+                <h2 className="select-none bg-gradient-to-t from-zinc-950 to-zinc-600 bg-clip-text pt-1 font-display-pacifico text-8xl text-transparent dark:from-zinc-500 dark:to-white">
                   Hi
-                </div>
-                <div className="font-mono text-xl dark:text-white lg:text-2xl">
-                  <p className="inline-flex">
-                    &lt;<span className="text-red-500">p</span>&gt;
-                    <span className="">{personalInfo.title}</span>
-                    &lt;/<span className="text-red-500">p</span>&gt;
-                  </p>
-                </div>
+                </h2>
+                <p className="font-mono text-xl lg:text-2xl dark:text-white">
+                  &lt;<span className="text-red-500">p</span>&gt;
+                  {personalInfo.title}
+                  &lt;/<span className="text-red-500">p</span>&gt;
+                </p>
               </div>
-              {/* About */}
+
+              {/* About Text */}
               <div className="text-balance text-center font-mono text-sm">
-                <p className="">
+                <p>
                   &lt;<span className="text-cyan-700">About</span>&gt;
-                  <span className="">{personalInfo.about}</span>
+                  {personalInfo.about}
                   &lt;/<span className="text-cyan-700">About</span>&gt;
                 </p>
               </div>
 
-              {/* Icons */}
-              <div className="backdrop-blur rounded-2xl p-4 shadow-sm flex gap-3">
+              {/* Tech Stack */}
+              <div className="backdrop-blur rounded-2xl p-4 shadow-sm flex gap-3 justify-center">
                 {technologies.map((tech) => (
                   <div
                     key={tech.name}
-                    className="w-10 h-10 p-1.5 bg-gray-200 rounded-full flex items-center justify-center hover:scale-130 transition-transform"
+                    className="w-10 h-10 p-1.5 bg-gray-200 rounded-full flex items-center justify-center hover:scale-110 transition-transform"
+                    title={tech.name}
                   >
                     <svg className="w-5 h-5">
                       <use href={`/icons/sprite.svg#${tech.icon}`} />
