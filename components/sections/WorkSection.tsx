f1b5248c147c8fae375adce409bd70162f1e71d3
Alex Huaracha <129701149+Alex-TheResilient@users.noreply.github.com>
2025-08-01 21:10:17 -0500

    Add opening and closing tags to WorkSection for improved semantic clarity

--- components/sections/WorkSection.tsx
+++ components/sections/WorkSection.tsx
@@ -6,9 +6,22 @@ export function WorkSection() {
 
   return (
     <div className="space-y-0">
+      {/* Open Tag */}
+      <span className="inline-flex font-mono ml-2 text-cyan-500 dark:text-cyan-400">
+        <span className="dark:text-zinc-50">&lt;</span>
+        <span>Work</span>
+        <span className="dark:text-zinc-50">&gt;</span>
+      </span>
+      {/* Content */}
       {workExperiences.map((experience, index) => (
         <ExperienceCard key={index} experience={experience} />
       ))}
+      {/* Close Tag */}
+      <span className="inline-flex font-mono ml-2 text-cyan-500 dark:text-cyan-400">
+        <span className="dark:text-zinc-50">&lt;/</span>
+        <span>Work</span>
+        <span className="dark:text-zinc-50">&gt;</span>
+      </span>
     </div>
   );
 }
